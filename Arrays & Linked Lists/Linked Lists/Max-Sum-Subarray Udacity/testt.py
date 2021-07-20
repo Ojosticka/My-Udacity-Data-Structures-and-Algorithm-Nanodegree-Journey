bf175c142243078817361bbfe85f02eebcf5a55e
Oluwatobi Ojo <oluwatobiojo453@gmail.com>
2021-07-20 17:52:57 +0100

    Max_Sum_Subarray Udacity

---
+++ Arrays & Linked Lists/Linked Lists/Max-Sum-Subarray Udacity/testt.py
@@ -0,0 +1,29 @@
+from Max_sum_subarray import max_sum_subarray
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    
+    output = max_sum_subarray(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+arr= [1, 2, 3, -4, 6]
+solution= 8 # sum of array
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [1, 2, -5, -4, 1, 6]
+solution = 7   # sum of last two elements
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [-12, 15, -13, 14, -1, 2, 1, -5, 4]
+solution = 18  # sum of subarray = [15, -13, 14, -1, 2, 1]
+
+test_case = [arr, solution]
+test_function(test_case)
