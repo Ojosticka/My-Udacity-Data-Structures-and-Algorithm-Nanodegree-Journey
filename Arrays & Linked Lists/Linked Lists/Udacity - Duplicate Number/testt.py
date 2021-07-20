37230aee7ca872c921e3e1886c8d2e0ea4ebb6d9
Oluwatobi Ojo <oluwatobiojo453@gmail.com>
2021-07-20 13:42:55 +0100

    Duplicate Number

---
+++ Arrays & Linked Lists/Linked Lists/Udacity - Duplicate Number/testt.py
@@ -0,0 +1,34 @@
+from duplicate import duplicate_number
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    output = duplicate_number(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+arr = [0, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 2, 3, 1, 4, 5, 3]
+solution = 3
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 4, 3, 2, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 5, 3, 2, 4]
+solution = 5
+
+test_case = [arr, solution]
+test_function(test_case)
