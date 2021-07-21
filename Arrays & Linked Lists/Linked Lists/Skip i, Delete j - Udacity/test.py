b99f437882a3f58f85348d5664aa29aad4891e8d
Oluwatobi Ojo <oluwatobiojo453@gmail.com>
2021-07-21 21:45:51 +0100

    skip i, delete j

---
+++ Arrays & Linked Lists/Linked Lists/Skip i, Delete j - Udacity/test.py
@@ -0,0 +1,72 @@
+from solution import Node, skip_i_delete_j
+
+# helper functions for testing purpose
+def create_linked_list(arr):
+    if len(arr)==0:
+        return None
+    head = Node(arr[0])
+    tail = head
+    for data in arr[1:]:
+        tail.next = Node(data)
+        tail = tail.next
+    return head
+
+def print_linked_list(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+
+#test function
+def test_function(test_case):
+    head = test_case[0]
+    i = test_case[1]
+    j = test_case[2]
+    solution = test_case[3]
+        
+    temp = skip_i_delete_j(head, i, j)
+    index = 0
+    try:
+        while temp is not None:
+            if temp.data != solution[index]:
+                print("Fail")
+                return
+            index += 1
+            temp = temp.next
+        print("Pass")
+    except Exception as e:
+        print("Fail")
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+i = 2
+j = 2
+head = create_linked_list(arr)
+solution = [1, 2, 5, 6, 9, 10]
+test_case = [head, i, j, solution]
+test_function(test_case)
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+i = 2
+j = 3
+head = create_linked_list(arr)
+solution = [1, 2, 6, 7, 11, 12]
+test_case = [head, i, j, solution]
+test_function(test_case)
+
+arr = [1, 2, 3, 4, 5]
+i = 2
+j = 4
+head = create_linked_list(arr)
+solution = [1, 2]
+test_case = [head, i, j, solution]
+test_function(test_case)
+
+arr = [1, 2, 3, 4, 5]
+i = 2
+j = 0
+head = create_linked_list(arr)
+solution = [1, 2, 3, 4, 5]
+test_case = [head, i, j, solution]
+test_function(test_case)
